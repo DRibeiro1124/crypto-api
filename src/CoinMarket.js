cad8f0cbdd5e97d55054ec0843532938b6dccdae
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-14 14:02:51 -0400

    added first component

---
+++ src/CoinMarket.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CoinMarket extends Component {
+    render () {
+        return (
+            <div className="App">
+            Updated currency data
+            </div>
+        )
+    }
+}
+
+export default CoinMarket
