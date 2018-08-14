aa16562c987ca438defa86a751bb734d36731512
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-14 17:40:07 -0400

    added url and fetched data

--- src/CoinMarket.js
+++ src/CoinMarket.js
@@ -1,13 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CoinMarket extends Component {
+
+constructor(props) {
+    super(props)
+    this.state = {
+        Id: 'loading...',
+        Name: '',
+        Symbol: '',
+        Rank: '',
+        Currency: '',
+        Updated: ''
+    }
+}    
+
+componentDidMount () {
+    const _url = "https://api.coinmarketcap.com/v2/ticker/?limit=20"
+    fetch(_url).then(resp => resp.json())
+    .then(newCoinMarket => {
+        console.log(newCoinMarket)
+    })
+}
+
+
     render () {
         return (
-            <div className="App">
-            Updated currency data
+            <div>
+                Current up to date Crypto Currency info:
+                <div>
+                {this.state.Id}
+                </div>
             </div>
-        )
+            
+               
+            
+        );
     }
 }
 
-export default CoinMarket
+export default CoinMarket;     
