8a708bea919b8ccdfec86c5134357d0d95012ee6
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-14 15:31:22 -0400

    added list component

---
+++ src/CoinMarketList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class CoinMarketList extends Component {
+    render () {
+        return (
+            <table>
+            <tr>ID</tr>
+            <tr>Name</tr>
+            <tr>Symbol</tr>
+            <tr>Price</tr>
+            <tr>Last Updated</tr>
+            </table>
+        )
+    }
