67a33863d1f4bcf6983e59778c9320ede2971629
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-15 15:01:46 -0400

    Project is finished, going to work on CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CoinData from './CoinData'
+import CryptoMarket from './CryptoMarket';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div>
-        <CoinData/>
-      </div>
+      <section className="App">
+      <h1>Up to date Crypto Currencies</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Icon</th>
+            <th className='currencyName'>Name</th>
+            <th>Symbol</th>
+            <th>Price</th>
+            <th className='hourlyChange'>Change Last HR</th>
+            <th className='dailyChange'>Change Last 24HR</th>
+          </tr>
+          <CryptoMarket />
+        </tbody>
+      </table>
+      </section>
     );
   }
-}  
+}
 
 export default App;
-
